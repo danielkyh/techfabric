8371b7584ce07d8f83d1c18e6f9f9015f90c1968
daniel <danielh714@gmail.com>
2018-05-12 09:04:17 -0700

    update GraphQL Vehicle Details Type

--- app/graphql/types/vehicle_details_type.rb
+++ app/graphql/types/vehicle_details_type.rb
@@ -1,8 +1,9 @@
 Types::VehicleDetailsType = GraphQL::ObjectType.define do
   name "Vehicle Details"
+	description "Vehicle Type Model"
 
-  description "Vehicle Type Model"
   field :id, !types.ID
-  field :model_id, !types.Int
+  field :vehicle_type_name, !types.String
+  field :vehicle_type_id, !types.Int
   field :make, Types::MakeType
 end
